97dfb7bb49525be7e687ac60ab0023008c096c7a
theloadstar <h0415am@163.com>
2021-09-08 11:08:21 +0800

    chapter15 initialization

---
+++ chapter15/FoodPin_15/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -0,0 +1,75 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Ember on 2021/9/3.
+//  Copyright © 2021 Ember. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var restaurant = Restaurant()
+    
+    @IBOutlet var tableView : UITableView!
+    @IBOutlet var headerView : RestanrantDetailHeaderView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+        headerView.headerImageView.image = UIImage(named: restaurant.image)
+        headerView.heartImageView.isHidden = !(restaurant.isVisited)
+        //connections
+        tableView.delegate = self
+        tableView.dataSource = self
+        //separate
+//        tableView.separatorStyle = .none
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: DataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(systemName: "phone")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+            cell.shortTextLabel.text = restaurant.phone
+            cell.selectionStyle = .default
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailIconTextCell.self), for: indexPath) as! RestaurantDetailIconTextCell
+            cell.iconImageView.image = UIImage(systemName: "map")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+            cell.shortTextLabel.text = restaurant.location
+            cell.selectionStyle = .none
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+            cell.descriptionLabel.text = restaurant.description
+            return cell
+        default:
+            fatalError("Failed to instantiate the table view cell for detail view controller")
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ chapter15/FoodPin_15/FoodPin/View/RestaurantTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Ember on 2021/8/31.
+//  Copyright © 2021 Ember. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel : UILabel!
+    @IBOutlet var locationLabel : UILabel!
+    @IBOutlet var typeLabel : UILabel!
+    @IBOutlet var thumbnailImageView : UIImageView!{
+        didSet{
+            thumbnailImageView.layer.cornerRadius = thumbnailImageView.layer.bounds.width/2.7;
+            //裁剪超出父视图的部分，这里可以不用，因为不会超出
+            thumbnailImageView.clipsToBounds = true;
+        }
+    }
+    @IBOutlet var HeartImageView : UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
